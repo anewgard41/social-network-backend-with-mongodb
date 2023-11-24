7cdf076d30af630318bb381e7a8c18ab1d51ef40
Anders Newgard <anne9707@colorado.edu>
2023-11-24 17:02:28 -0500

    set up User model. Added virtual to keep track of friend count.

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return `/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/`.test(v);
+            },
+            message: "Please enter a valid email"
+        },
+        required: [true, "Email required"]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: "Thought"
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }]
+
+});
+
+userSchema.virtual("friendCount").get(function() {
+    return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
